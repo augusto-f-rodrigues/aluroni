9b5ac82ecb80b7fe9638067fd208c013ce889bc8
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2022-12-29 14:11:46 -0300

    feat: Primeiro Commit'

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./Botao.module.scss"
+
+export default function Botao ()  {
+  return (
+    <button className={styles.botao}>
+      Botão
+    </button>
+  )
+}
---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,12 @@
+import Botao from "../../components/Botao"
+import styles from "./Cardapio.module.scss"
+
+export default function Cardapio () {
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <img src="" alt="logo-aluroni" />
+      </nav>
+    </main>
+  )
+}
