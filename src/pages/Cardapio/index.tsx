1cfe175ba122fdd8604e7c1a7184dac3640dfbd4
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2022-12-30 11:08:44 -0300

    feat: Criando o header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,12 +1,16 @@
-import Botao from "../../components/Botao"
-import styles from "./Cardapio.module.scss"
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
 
-export default function Cardapio () {
+export default function Cardapio() {
   return (
     <main>
       <nav className={styles.menu}>
-        <img src="" alt="logo-aluroni" />
+        <Logo />
       </nav>
+
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código e da massa</div>
+      </header>
     </main>
-  )
+  );
 }
