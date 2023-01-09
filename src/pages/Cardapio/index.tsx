450fcdead54a572db70de22d1409c18cdfe2d338
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2023-01-09 14:05:46 -0300

    feat: component Filtro finalizado

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -6,24 +6,20 @@ import Filtros from "./Filtros";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
-
+  const [filtro, setFiltro] = useState<number | null>(null);
   return (
     <main>
       <nav className={styles.menu}>
         <Logo />
       </nav>
-
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
-
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-
         <Buscador busca={busca} setBusca={setBusca} />
-
         <div className={styles.cardapio__filtros}>
-          <Filtros/>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
         </div>
       </section>
     </main>
