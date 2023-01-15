9a452c9f13cbb132a4461f3f44bc71b11d6104ca
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2023-01-15 11:29:52 -0300

    feat: Adicionando as imagens na public

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -7,7 +7,7 @@ function Itens() {
   return (
     <div className={styles.itens}>
       {cardapio.map((item) => (
-        <Item key={item.id} />
+        <Item key={item.id} {...item} />
       ))}
     </div>
   );
